64356465c6485b908db1bcb915d46b94f3d8c3b4
Himanshu12145 <himansu12145@gmail.com>
2023-02-27 11:08:23 +0530

    Dummy Data Added

--- components/ExpensesOutput/ExpensesSummary.jsx
+++ components/ExpensesOutput/ExpensesSummary.jsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const ExpensesSummary = () => {
+const ExpensesSummary = ({ expenses, periodName }) => {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
   return (
     <View>
-      <Text>Last 7 days</Text>
-      <Text>$12,33</Text>
+      <Text>{periodName}</Text>
+      <Text>₹{expensesSum.toFixed(2)}</Text>
     </View>
   );
 };
