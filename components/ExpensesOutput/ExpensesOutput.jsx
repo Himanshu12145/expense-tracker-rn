2c78e99e3ab42802138df263dc7b8db865811a06
Himanshu12145 <himansu12145@gmail.com>
2023-02-27 10:41:03 +0530

    added the basic navigation setup

---
+++ components/ExpensesOutput/ExpensesOutput.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ExpensesOutput = () => {
+  return (
+    <View>
+      <Text>ExpensesOutput</Text>
+    </View>
+  );
+};
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({});
